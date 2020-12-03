0aa58e831c4e9a8df6ce416255e6916eb8d2fbae
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-12-03 19:11:26 +0200

    Date

--- script.js
+++ script.js
@@ -8,30 +8,47 @@ const fetchJSONFile = (path, callback) => {
       }
     }
   };
-  httpRequest.open('GET', path);
+  httpRequest.open("GET", path);
   httpRequest.send();
 };
 
-fetchJSONFile('libs.json', (libs) => {
-  const template = document.getElementById('template');
+const getDate = (date) => {
+  const d = new Date(date);
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  return `${d.getDate()} ${MONTHS[d.getMonth()]} ${d.getFullYear()}`;
+};
+
+fetchJSONFile("libs.json", (libs) => {
+  const template = document.getElementById("template");
   for (lib of libs) {
-    const divLib = document.createElement('div');
-    divLib.classList.add('lib');
+    const divLib = document.createElement("div");
+    divLib.classList.add("lib");
     let inner = template.innerHTML;
 
     const source = `libs/${lib.source}`;
-    inner = inner.replace('{name}', lib.name);
-    inner = inner.replace('{description}', lib.description);
-    inner = inner.replace('{source}', source);
-    inner = inner.replace('{url}', lib.url);
-    inner = inner.replace('{author}', lib.author);
-    inner = inner.replace('{preview}', `libs/${lib.preview}`);
+    inner = inner.replace("{name}", lib.name);
+    inner = inner.replace("{description}", lib.description);
+    inner = inner.replace("{source}", source);
+    inner = inner.replace("{url}", lib.url);
+    inner = inner.replace("{author}", lib.author);
+    inner = inner.replace("{preview}", `libs/${lib.preview}`);
+    inner = inner.replace("{updated}", getDate(lib.date));
     inner = inner.replace(
-      '{addToLib}',
-      `https://excalidraw.com/?addLibrary=${location.href.replace(
-        'index.html',
-        '',
-      )}${source}`,
+      "{addToLib}",
+      `https://excalidraw.com/?addLibrary=${location.href.replace("index.html", "")}${source}`
     );
     divLib.innerHTML = inner;
     template.after(divLib);
