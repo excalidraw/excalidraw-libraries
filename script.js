2d7289677ed716e625ca9583e28b039af601d9a9
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-17 16:09:14 +0200

    Update stats

--- script.js
+++ script.js
@@ -31,32 +31,61 @@ const getDate = (date) => {
   return `${d.getDate()} ${MONTHS[d.getMonth()]} ${d.getFullYear()}`;
 };
 
-fetchJSONFile("libraries.json", (libraries) => {
-  const template = document.getElementById("template");
-  for (library of libraries) {
-    const divLib = document.createElement("div");
-    divLib.classList.add("library");
-    const replaceText = { "/": "-", ".excalidrawlib": "" };
-    const divId = library.source.replace(/\/|.excalidrawlib/g, function (match) {
-      return replaceText[match];
-    });
-    divLib.setAttribute("id", divId);
-    let inner = template.innerHTML;
+const DAY = 24 * 60 * 60 * 1000;
 
-    const source = `libraries/${library.source}`;
-    inner = inner.replace(/\{libraryId\}/g, divId);
-    inner = inner.replace(/\{name\}/g, library.name);
-    inner = inner.replace(/\{description\}/g, library.description);
-    inner = inner.replace(/\{source\}/g, source);
-    inner = inner.replace(/\{url\}/g, library.url);
-    inner = inner.replace(/\{author\}/g, library.author);
-    inner = inner.replace(/\{preview\}/g, `libraries/${library.preview}`);
-    inner = inner.replace(/\{updated\}/g, getDate(library.date));
-    inner = inner.replace(
-      "{addToLib}",
-      `https://excalidraw.com/?addLibrary=${location.href.replace("index.html", "")}${source}`
-    );
-    divLib.innerHTML = inner;
-    template.after(divLib);
-  }
+fetchJSONFile("libraries.json", (libraries) => {
+  fetchJSONFile("stats.json", (stats) => {
+    const template = document.getElementById("template");
+    const divElements = [];
+    const libraries_ = [];
+    for (library of libraries) {
+      const replaceText = { "/": "-", ".excalidrawlib": "" };
+      const libraryId = library.source.replace(/\/|.excalidrawlib/g, function (match) {
+        return replaceText[match];
+      });
+      library["id"] = libraryId;
+      library["downloads"] = {
+        total: stats[libraryId].total,
+        week: stats[libraryId].week,
+      };
+
+      libraries_.push(library);
+    }
+
+    libraries_.sort((a, b) => {
+      const aTime = new Date(a.date);
+      const today = new Date();
+      const diffA = (today.getTime() - aTime.getTime()) / DAY;
+      if (diffA < 14) {
+        return 1;
+      }
+      return a.downloads.week - b.downloads.week;
+    });
+
+    console.log(libraries_);
+
+    for (library of libraries_) {
+      const div = document.createElement("div");
+      div.classList.add("library");
+      div.setAttribute("id", library.id);
+      let inner = template.innerHTML;
+      const source = `libraries/${library.source}`;
+      inner = inner.replace(/\{libraryId\}/g, library.id);
+      inner = inner.replace(/\{name\}/g, library.name);
+      inner = inner.replace(/\{description\}/g, library.description);
+      inner = inner.replace(/\{source\}/g, source);
+      inner = inner.replace(/\{url\}/g, library.url);
+      inner = inner.replace(/\{author\}/g, library.author);
+      inner = inner.replace(/\{preview\}/g, `libraries/${library.preview}`);
+      inner = inner.replace(/\{updated\}/g, getDate(library.date));
+      inner = inner.replace(
+        "{addToLib}",
+        `https://excalidraw.com/?addLibrary=${location.href.replace("index.html", "")}${source}`
+      );
+      inner = inner.replace(/\{total\}/g, library.downloads.total);
+      inner = inner.replace(/\{week\}/g, library.downloads.week);
+      div.innerHTML = inner;
+      template.after(div);
+    }
+  });
 });
