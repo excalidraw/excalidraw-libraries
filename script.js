412d2794626449b66058d5f6819b7682231e664e
Aravindhan <madebyhuman@outlook.com>
2021-05-26 19:15:06 +0530

    Show the current year for the Copyright in the footer

--- script.js
+++ script.js
@@ -222,3 +222,7 @@ fetchJSONFile("libraries.json", (libraries) => {
     scrollToAnchor();
   });
 });
+
+// update footer with current year
+const footer = document.getElementById("footer");
+footer.innerHTML = footer.innerHTML.replace(/{currentYear}/g, () => new Date().getFullYear());
