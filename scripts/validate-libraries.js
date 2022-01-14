419f6196024bcbafa00bd6e820364f241ddfa041
dwelle <luzar.david@gmail.com>
2022-01-14 22:58:43 +0100

    assert non-empty of all basic fields

--- scripts/validate-libraries.js
+++ scripts/validate-libraries.js
@@ -2,8 +2,19 @@ const librariesList = require("../libraries.json");
 
 const red = (str) => (process.env.CI ? `\x1b[31m${str}\x1b[0m` : str);
 
-for (const lib of librariesList) {
-  if (!lib.version) {
-    throw new Error(red(`"${lib.source}" doesn't have "version" field set.`));
+const assertNonEmpty = (lib, field) => {
+  if (!lib[field]) {
+    throw new Error(red(`"${lib.source}" doesn't have "${field}" field set.`));
   }
+};
+
+for (const lib of librariesList) {
+  assertNonEmpty(lib, "name");
+  assertNonEmpty(lib, "description");
+  assertNonEmpty(lib, "version");
+  assertNonEmpty(lib, "source");
+  assertNonEmpty(lib, "preview");
+  assertNonEmpty(lib, "created");
+  assertNonEmpty(lib, "updated");
+  assertNonEmpty(lib, "authors");
 }
