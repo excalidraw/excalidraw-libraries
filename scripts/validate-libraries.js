9c2db9cf69ca3b843d5e5c8676272dd66dcd635e
dwelle <luzar.david@gmail.com>
2022-03-28 17:04:03 +0200

    feat: add and require library ids

--- scripts/validate-libraries.js
+++ scripts/validate-libraries.js
@@ -1,4 +1,6 @@
-const librariesList = require("../libraries.json");
+const _ = require("lodash");
+
+const libraries = require("../libraries.json");
 
 const red = (str) => (process.env.CI ? `\x1b[31m${str}\x1b[0m` : str);
 
@@ -8,7 +10,40 @@ const assertNonEmpty = (lib, field) => {
   }
 };
 
-for (const lib of librariesList) {
+const assertExistingIds = () => {
+  const libsWithNoId = [];
+  for (const lib of libraries) {
+    if (!lib.id) {
+      libsWithNoId.push(lib);
+    }
+  }
+  if (libsWithNoId.length) {
+    throw new Error(
+      red(
+        `Following libraries don't have an "id" field:\n\t"${libsWithNoId
+          .map((lib) => lib.name)
+          .join(`"\n\t"`)}"`,
+      ),
+    );
+  }
+};
+
+const assertUniqueIds = () => {
+  const groups = _.groupBy(libraries, "id");
+  const duplicateIds = [];
+  for (const [id, libs] of Object.entries(groups)) {
+    if (libs.length > 1) {
+      duplicateIds.push(id);
+    }
+  }
+  if (duplicateIds.length) {
+    throw new Error(red(`Found duplicate ids: "${duplicateIds.join(`", "`)}"`));
+  }
+};
+
+// -----------------------------------------------------------------------------
+
+for (const lib of libraries) {
   assertNonEmpty(lib, "name");
   assertNonEmpty(lib, "description");
   assertNonEmpty(lib, "version");
@@ -18,3 +53,6 @@ for (const lib of librariesList) {
   assertNonEmpty(lib, "updated");
   assertNonEmpty(lib, "authors");
 }
+
+assertExistingIds();
+assertUniqueIds();
